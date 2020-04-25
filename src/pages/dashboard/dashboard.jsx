93192bdb31cbad2116541774f986471668a9e55a
Darken141 <Darken141@github.com>
2020-04-25 13:49:23 +0200

    mock up dashboard

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { FaBars, FaUserCircle, FaTimes } from 'react-icons/fa';
 
 import './dashboard.styles.scss';
 
 const Dashboard = () => {
+	const [ toggleAside, setToggleAside ] = useState(false);
+
 	return (
 		<div id="dashboard">
-			<h1>Dashboard...</h1>
-			<p>
-				Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean
-				collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
-			</p>
+			<header id="dashboard__header">
+				<div className="logo__container">
+					<h1 className="logo">CheckVid.io</h1>
+				</div>
+				<div className="menu-items">
+					<div className="menu-icon" onClick={() => setToggleAside(!toggleAside)}>
+						<FaBars />
+					</div>
+
+					<div className="user-menu-icon">
+						<FaUserCircle />
+					</div>
+				</div>
+			</header>
+			<aside id="dashboard__aside" className={toggleAside ? 'active' : ''}>
+				ASIDE
+				<div className="close-icon" onClick={() => setToggleAside(false)}>
+					<FaTimes />
+				</div>
+			</aside>
+			<main id="dashboard__main">
+				<section id="projects">
+					<h1>Projects</h1>
+				</section>
+			</main>
+			<footer id="dashboard__footer">
+				<div>CODERKIN</div>
+
+				<div>SOCIAL MEDIA</div>
+			</footer>
 		</div>
 	);
 };
