8a7d9a560fa4bf2da41d405d44bb1110c2a957e4
Darken141 <Darken141@github.com>
2020-05-19 01:04:00 +0200

    add link to footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -8,7 +8,12 @@ const Footer = () => {
 	return (
 		<footer id="footer">
 			<div className="row">
-				<div className="col">&copy; CODERKIN</div>
+				<div className="col">
+					&copy;{' '}
+					<a href="https://www.coderkin.com" target="_blank" rel="noopener noreferrer">
+						CODERKIN
+					</a>
+				</div>
 				<div className="col icons">
 					<a href="https://www.facebook.com/CheckVidio-102042618125958" className="facebook-icon">
 						<FaFacebookSquare />
