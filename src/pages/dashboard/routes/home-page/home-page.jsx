3fc9bb1b86448bb04803a30a317249c4b656001d
Darken141 <Darken141@github.com>
2020-05-28 21:48:35 +0200

    refactor

--- src/pages/dashboard/routes/home-page/home-page.jsx
+++ src/pages/dashboard/routes/home-page/home-page.jsx
@@ -23,34 +23,6 @@ const HomePage = () => {
 					<h2 className="sub-heading">Ahoj</h2>
 					<h1 className="heading">{displayName}</h1>
 				</div>
-
-				<div className="news">
-					<h2 className="heading">Novinky</h2>
-					<div className="new">
-						<div className="new__heading">
-							<h3 className="sub-heading">Vitajte na CheckVid.io</h3>
-							<p className="new__heading--time">26. máj 2020</p>
-						</div>
-						<p>
-							Práve sa nachádzame predbežnom prístupe našej platformy pre kameramanov a freelancerov. Naša
-							platforma vám umožní efektívne zberať feedback od vašich klientov.
-						</p>
-						<ul>
-							<li>
-								<Link to={`${match.url}/create-project`}>Vytvárajte</Link> video projekty
-							</li>
-							<li>
-								<Link to={`${match.url}/projects`}>Posielajte</Link> emaily klientom
-							</li>
-							<li>
-								Zberajte feedback a podeľte sa s nami o jeden tiež a{' '}
-								<a href="https://checkvid.io/contact" target="_blank" rel="noopener noreferrer">
-									Napište nám
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
 			</div>
 
 			<h2 className="heading label-heading">Navigácia</h2>
